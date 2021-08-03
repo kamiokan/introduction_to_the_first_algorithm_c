856f398adb0d38852f98341f6c1c8c83e5e0e240
kamiokan <spherenov@gmail.com>
2021-08-04 08:14:42 +0900

    コードフォーマットした

--- p13.c
+++ p13.c
@@ -1,27 +1,23 @@
 /*
-* -------------------------
-* 漸化式（nCrの計算）
-* -------------------------
-*/
+ * -------------------------
+ * 漸化式（nCrの計算）
+ * -------------------------
+ */
 #include <stdio.h>
 
-long combi(int, int);
+long combine(int, int);
 
-int main(void)
-{
-    int n,r;
+int main(void) {
+  int n, r;
 
-    for (n=0;n<=5;n++){
-        for (r=0;r<=n;r++)
-            printf("%dC%d=%ld  ",n,r,combi(n,r));
-        printf("\n");
-    }
+  for (n = 0; n <= 5; n++) {
+    for (r = 0; r <= n; r++) printf("%dC%d=%ld  ", n, r, combine(n, r));
+    printf("\n");
+  }
 }
-long combi(int n, int r) 
-{
-    int i;
-    long p=1;
-    for(i=1;i<=r;i++)
-        p=p*(n-i+1)/i;
-    return p;
-} 
+long combine(int n, int r) {
+  int i;
+  long p = 1;
+  for (i = 1; i <= r; i++) p = p * (n - i + 1) / i;
+  return p;
+}
