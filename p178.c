92f329319ecf1557679e2c843149926b85f69db8
kamiokan <spherenov@gmail.com>
2021-10-11 07:44:06 +0900

    階乗計算の非再帰版

---
+++ p178.c
@@ -0,0 +1,20 @@
+/*
+ * -------------------------
+ * 階乗計算の非再帰版
+ * -------------------------
+ */
+#include <stdio.h>
+
+long kaijo(int);
+
+int main(void) {
+  int n;
+
+  for (n = 0; n < 13; n++) printf("%2d!= %10ld\n", n, kaijo(n));
+}
+long kaijo(int n) {
+  int k;
+  long p = 1L;
+  for (k = n; k >= 1; k--) p = p * k;
+  return p;
+}
